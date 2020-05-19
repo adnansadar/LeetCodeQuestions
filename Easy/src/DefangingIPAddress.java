7f50a66e36c5aee48c12cfa8d24493fbe8b5d468
adnansadar <adnansadar11@gmail.com>
2020-05-19 07:03:05 +0530

    adding new program

---
+++ Easy/src/DefangingIPAddress.java
@@ -0,0 +1,14 @@
+public class DefangingIPAddress {
+
+    public static String defangIPaddr(String[] address) {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < address.length(); i++){
+            if (address.charAt(i) == '.'){
+                str.append("[.]");
+            } else {
+                str.append(address.charAt(i));
+            }
+        }
+        return str.toString();
+    }
+}
--- Easy/src/SubtreeofAnotherTree.java
+++ Easy/src/SubtreeofAnotherTree.java
@@ -1,6 +1,6 @@
 
 public class SubtreeofAnotherTree {
-    public static  class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -9,21 +9,21 @@ public class SubtreeofAnotherTree {
         }
     }
     public static boolean isSubtree(TreeNode s, TreeNode t) {
-        if (s == null) {
+        if (s == null) {//base condition for recursion
             return false;
-        } else if (isSameTree(s, t)) {
+        } else if (isSameTree(s, t)) { //if any subtree is equal start checking and return true
             return true;
         } else {
-            return isSubtree(s.left, t) || isSubtree(s.right, t);
+            return isSubtree(s.left, t) || isSubtree(s.right, t); //traversing the left and right nodes of the t tree
         }
     }
 
     public static boolean isSameTree(TreeNode s, TreeNode t){
-        if(s==null || t==null){
+        if(s==null || t==null){ //base condition to exit recursion
             return s == null && t==null;
         }
         else if(s.val == t.val){
-            return isSameTree(s.left,t.left) && isSameTree(s.right,t.right);
+            return isSameTree(s.left,t.left) && isSameTree(s.right,t.right); //if both are equal then only
         }
         else{
             return false;
